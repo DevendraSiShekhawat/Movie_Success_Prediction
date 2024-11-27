a827e7fbc76d00295de01072d50d4eea61343599
DevendraSiShekhawat <165901991+DevendraSiShekhawat@users.noreply.github.com>
2024-11-27 21:53:41 +0530

    Update settings.py

--- mvs/settings.py
+++ mvs/settings.py
@@ -79,10 +79,15 @@ WSGI_APPLICATION = 'mvs.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'your_db_name',
+        'USER': 'your_db_user',
+        'PASSWORD': 'your_db_password',
+        'HOST': 'db',  # This will reference the db service in docker-compose
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
